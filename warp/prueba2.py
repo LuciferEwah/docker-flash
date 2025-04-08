8e8e73f19e191eda5f4b4ed20231e328561c6319
AxelRamirezDatawalt <axel.ramirez@datawalt.cl>
2025-04-07 23:31:51 -0400

    configuration

---
+++ warp/prueba2.py
@@ -0,0 +1,23 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    var1 = os.getenv('VAR1', 'ENCONTRADOooooo')
+    var2 = os.getenv('VAR2', 'VALOR 2 NO ENCONTRADO')
+
+    return f"""
+    <html>
+        <head><title>Variables de Entorno DESDE PRUEBA2</title></head>
+        <body>
+            <h1>Variables desde Dockerrrr</h1>
+            <p><strong>VAR1:</strong> {var1}</p>
+            <p><strong>VAR2:</strong> {var2}</p>
+        </body>
+    </html>
+    """
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)  # 👈 esto activa live reload
